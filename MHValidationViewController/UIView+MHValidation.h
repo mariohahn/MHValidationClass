57ca91355477e91383cda464e87cbc5dd9abef48
Mario Hahn <mh@tailored-apps.com>
2013-07-24 09:57:59 +0200

    Added CustomizationBlock

--- MHValidationViewController/UIView+MHValidation.h
+++ MHValidationViewController/UIView+MHValidation.h
@@ -24,27 +24,111 @@ typedef NS_ENUM(NSUInteger, MHSelectionType) {
     MHSelectionTypeCurrent
 };
 
+
+
+@interface MHTextObjectsCustomization : NSObject
+
+
+@property (nonatomic,strong) NSArray *classes;
+@property (nonatomic) float cornerRadius;
+@property (nonatomic) float borderWidth;
+@property (nonatomic,strong) UIColor *backgroundColor;
+@property (nonatomic,strong) UIColor *selectionBackgroundColor;
+
+@property (nonatomic,strong) UIColor *borderGradientColorUp;
+@property (nonatomic,strong) UIColor *borderGradientColorDow;
+
+@property (nonatomic,strong) UIColor *selectionGradientBorderColorUp;
+@property (nonatomic,strong) UIColor *selectionGradientBorderColorDown;
+
+@property (nonatomic,strong) UIColor *innerShadowColor;
+@property (nonatomic,strong) UIColor *selectionInnerShadowColor;
+
+@property (nonatomic,strong) UIFont *labelFont;
+@property (nonatomic,strong) UIColor *labelColor;
+@property (nonatomic,strong) UIColor *selectionLabelColor;
+
+
+
+- (id)initWithClassesForCustomization:(NSArray*)classes
+                      backgroundColor:(UIColor*)backgroundColor
+             selectionBackgroundColor:(UIColor*)selectionBackgroundColor
+                borderGradientColorUp:(UIColor*)borderGradientColorUp
+               borderGradientColorDow:(UIColor*)borderGradientColorDow
+       selectionGradientBorderColorUp:(UIColor*)selectionGradientBorderColorUp
+     selectionGradientBorderColorDown:(UIColor*)selectionGradientBorderColorDown
+                          borderWidth:(float)borderWidth
+                         cornerRadius:(float)cornerRadius
+                     innerShadowColor:(UIColor*)innerShadowColor
+            selectionInnerShadowColor:(UIColor*)selectionInnerShadowColor
+                           labelColor:(UIColor*)labelColor
+                            labelFont:(UIFont*)labelFont
+                  selectionLabelColor:(UIColor*)selectionLabelColor;
+
+@end
+
+@interface MHTextView : UIView
+@property (nonatomic,strong) MHTextObjectsCustomization *customization;
+@property (nonatomic) BOOL isSelected;
+-(id)initWithFrame:(CGRect)frame
+     customization:(MHTextObjectsCustomization*)customization
+        isSelected:(BOOL)isSelected;
+@end
+
+
+
+
 @interface MHValidationItem : NSObject
 @property (nonatomic, strong) id object;
 @property (nonatomic,strong) NSString *regexString;
 
-- (id)initWithObject:(id)object regexString:(NSString*)regexString;
+- (id)initWithObject:(id)object
+         regexString:(NSString*)regexString;
 @end
 
 
-@interface UIView (MHValidation)<UITextFieldDelegate>
+
+
+
+@interface UIView (MHValidation)<UITextFieldDelegate,UITextViewDelegate>
 @property (nonatomic, copy) NSArray *classObjects;
-@property (nonatomic) BOOL shouldShowNextPrevWithToolbar;
+@property (nonatomic) BOOL showNextAndPrevSegmentedControl;
 @property (nonatomic) BOOL shouldShakeNonValidateObjects;
+@property (nonatomic,copy) MHTextObjectsCustomization *textObjectsCustomization;
 
 
--(void)selectFieldWithSelectedObject:(id)selectedObject searchForObjectsOfClass:(NSArray*)classes selectNextOrPrevObject:(MHSelectionType)selectionType foundObjectBlock:(void(^)(id object, MHSelectedObjectType objectType ))FoundObjectBlock;
+-(void)searchForObjectsOfClass:(NSArray*)classes
+        selectNextOrPrevObject:(MHSelectionType)selectionType
+              foundObjectBlock:(void(^)(id object,
+                                        MHSelectedObjectType objectType )
+                                )FoundObjectBlock;
 
--(void)validateWithNonMandatoryField:(NSArray*)nonMandatoryFields andShouldValidateObjectsWithMHRegexObjects:(NSArray*)regexObject switchesWhichMustBeON:(NSArray*)onSwitches curruptObjectBlock:(void(^)(NSArray *curruptItem))CurruptedObjectBlock successBlock:(void(^)(NSString *emailString,NSDictionary *valueKeyEmail,NSArray *object,bool isFirstRegistration))SuccessBlock;
-- (void)shakeObjects:(id)objects andChangeBorderColor:(UIColor*)borderColor;
+-(void)validateWithNONMandatoryTextObjects:(NSArray*)nonMandatoryFields
+         validateObjectsWithMHRegexObjects:(NSArray*)regexObject
+                     switchesWhichMustBeON:(NSArray*)onSwitches
+                        curruptObjectBlock:(void(^)(NSArray *curruptItem)
+                                            )CurruptedObjectBlock
+                              successBlock:(void(^)(NSString *emailString,
+                                                    NSDictionary *valueKeyDict,
+                                                    NSArray *object,
+                                                    bool isFirstRegistration)
+                                            )SuccessBlock;
 
--(NSArray*)findObjectsofClass:(NSArray*)classArray onView:(UIView*)view andShowOnlyNonHiddenObjects:(BOOL)nonHidden;
 
--(void)initMHValidationWithClassObjectsToValidate:(NSArray*)classObjects;
+
+
+- (void)shakeObjects:(id)objects
+    shakeBorderColor:(UIColor*)borderColor;
+
+-(NSArray*)findObjectsofClass:(NSArray*)classArray
+                       onView:(UIView*)view
+     showOnlyNonHiddenObjects:(BOOL)nonHidden;
+
+-(void)installMHValidationWithClasses:(NSArray*)typeOfClasses
+                setCustomizationBlock:(void(^)(MHTextObjectsCustomization *customization))CustomizationBlock;
+
+
+
+
 
 @end
--- MHValidationViewController/ValidationViewController.m
+++ MHValidationViewController/ValidationViewController.m
@@ -29,31 +29,52 @@
     self.PLZ.accessibilityIdentifier = @"Postleitzahl";
     self.sex.accessibilityIdentifier = @"Geschlecht";
     self.allow.accessibilityIdentifier = @"Erlauben";
+    self.problems.accessibilityIdentifier = @"Probleme";
+
     
     
-    [self.scrollView initMHValidationWithClassObjectsToValidate:@[[UITextField class],[UISwitch class],[UISegmentedControl class]]];
-    [self.scrollView setShouldShowNextPrevWithToolbar:NO];
     
+    
+    
+    
+    
+    
+    [self.scrollView installMHValidationWithClasses:@[[UITextField class],
+                                                      [UISwitch class],
+                                                      [UISegmentedControl class],
+                                                      [UITextView class]
+                                                        ]
+                           setCustomizationBlock:^(MHTextObjectsCustomization *customization) {
+                            
+                           }];
+    
+    
+    
+    [self.scrollView setShouldShakeNonValidateObjects:YES];
+    [self.scrollView setShowNextAndPrevSegmentedControl:YES];
     
 }
 
 -(void)validateButtonAction{
     
-    MHValidationItem *emailValidation = [[MHValidationItem alloc]initWithObject:self.email regexString:MHValidationRegexEmail];
+    MHValidationItem *emailValidation = [[MHValidationItem alloc]initWithObject:self.email
+                                                                    regexString:MHValidationRegexEmail];
     
-    [self.scrollView validateWithNonMandatoryField:@[self.secondName]
-        andShouldValidateObjectsWithMHRegexObjects:@[emailValidation]
-                          switchesWhichMustBeON:nil
-                                curruptObjectBlock:^(NSArray *curruptItem) {
-                                        [self.scrollView shakeObjects:curruptItem andChangeBorderColor:nil];
-                            
-                                    } successBlock:^(NSString *emailString, NSDictionary *valueKeyEmail, NSArray *object, bool isFirstRegistration) {
-                                
-                                NSLog(@"%@",emailString);
-                                NSLog(@"%@",valueKeyEmail);
-                                NSLog(@"%@",object);
-        
-                                }];
+    
+    
+    [self.scrollView validateWithNONMandatoryTextObjects:@[self.secondName]
+                       validateObjectsWithMHRegexObjects:@[emailValidation]
+                                   switchesWhichMustBeON:nil
+                                      curruptObjectBlock:^(NSArray *curruptItem) {
+                                        //  [self.scrollView shakeObjects:curruptItem shakeBorderColor:nil];
+                                          
+                                      } successBlock:^(NSString *emailString, NSDictionary *valueKeyDict, NSArray *object, bool isFirstRegistration) {
+                                          
+                                          NSLog(@"%@",emailString);
+                                          NSLog(@"%@",valueKeyDict);
+                                          NSLog(@"%@",object);
+                                          
+                                      }];
 }
 
 
